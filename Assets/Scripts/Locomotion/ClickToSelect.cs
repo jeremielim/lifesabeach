e3cc932769e05d8a74c3114d06e4157ec96fdfaf
Jeremie Lim <hello@jeremiespoken.com>
2017-03-25 00:04:14 -0700

    Add click to select script

---
+++ Assets/Scripts/Locomotion/ClickToSelect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ClickToSelect : MonoBehaviour
+{
+
+    RaycastHit hitInfo = new RaycastHit();
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+        {
+            Debug.Log(hitInfo.transform.gameObject);
+        }
+    }
+}
